1816087e6bf066fe2972d55f6f391da93e905931
Ken Lu <ken@seeice.com>
2013-04-08 09:17:17 -0400

    project folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blaze::Application.config.secret_token = '2ab87141682adc005be5516b7eb2a1f10d16a0b3a91074a0d07e0386311cc9e00d49557f668f5f9188f47ed3633580cbb9f56e3ee5146d035bce2b1adad596e3'
