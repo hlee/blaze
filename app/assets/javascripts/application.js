8e6d7f1113ff70cd2d6c19cbebb0958ec5b4cb94
songjiayang <songjiayang1@gmail.com>
2013-04-14 04:43:12 +0800

    jquery.smooth-scroll is so good

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,8 @@
 //= require jquery_ujs
 //= require_tree .
 
-
 $(function(){
+	$('.menu li a').smoothScroll();
 	$(".menu li  a").click(function(){
 		$(".menu li a").removeClass('current');
 		$(this).addClass('current');
