1b3eba5daec0c97b035fbad9c440d360e1d5954a
Ken Lu <ken@seeice.com>
2013-04-23 09:26:04 -0400

    added digest authenticate

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,16 @@
 class AdminController < ApplicationController
-	http_basic_authenticate_with :name => "songjiayang", :password => "liushan1314"
+  USERS = {"songjiayang" => "liushan1314", "ken" => "password123"}
+
+  before_filter :authenticate
   def index
   	@projects = Project.all
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
   end
 end
